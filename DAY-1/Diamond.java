61ab57618b6343f1980d15be7871367267e3052c
Prajwal Devali <prajwaldevali9@gmail.com>
2026-04-15 23:22:25 +0530

    creation

---
+++ DAY-1/Diamond.java
@@ -0,0 +1,45 @@
+//----pattern printing diamond----
+
+import java.util.Scanner;
+public class Diamond{
+    
+    public static void diamond(int n){
+
+        //first half
+        for(int i=1; i<=n; i++){
+            //gap
+            for(int j=1; j<=n-i; j++){
+                System.out.print("-");
+            }
+            //star
+            for(int k=1; k<=i+(i-1); k++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        //sec half (inverted of first half)
+        for(int i=n-1; i>=1; i--){
+            //gap
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int k=1; k<=i+(i-1); k++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        
+    }
+
+    //main
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter size of diamond: ");
+        diamond(sc.nextInt());
+    }
+}
