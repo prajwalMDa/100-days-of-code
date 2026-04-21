41138e1f9e4fedd9fe2b913b64eefd9a154a9652
Prajwal Devali <prajwaldevali9@gmail.com>
2026-04-22 02:52:39 +0530

    day 7

---
+++ DAY-7/Solution3.java
@@ -0,0 +1,12 @@
+class Solution3 {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n%2==0){
+            n/=2;
+            
+        }
+        return n==1;
+    }
+}
